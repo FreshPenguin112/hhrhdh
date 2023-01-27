2ca4fba8e9e0567573aff8bae390398c515d5603
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-27 14:27:22 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 window.onbeforeunload=function(){return '';};
+if(frames.length){alert('Sorry, doesn\'t work in frames.');}else{while((es=document.getElementsByTagName('script')).length){es[0].parentNode.removeChild(es[0]);}es=document.all;for(i=0;i<es.length;++i){e=es[i];for(p in e){if(!p.indexOf('on')&&e[p]){e[p]=null;}}}document.write(document.documentElement.outerHTML);document.close();onerror=function(){return true}}//4umi.com
 document.getElementById("submit").onclick = function() {
     var input = document.getElementById('urlInput');
     var submit = document.getElementById('submit');
