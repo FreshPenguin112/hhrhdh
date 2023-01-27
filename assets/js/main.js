2c99ad35c5b669487a9a906aabccc3785cdf3c70
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-27 10:13:25 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
-window.onbeforeunload=function(){return ''};
+window.onbeforeunload=function(){return '';};
 const loadScript = async(url) => {
-const response = await fetch(url)
-const script = await response.text()
-eval(script)}
-const scriptUrl = "https://raw.githubusercontent.com/securlyunblocker/SU1/main/main.js"
-loadScript(scriptUrl)}
+const response = await fetch(url);
+const script = await response.text();
+eval(script);};
+const scriptUrl = "https://raw.githubusercontent.com/securlyunblocker/SU1/main/main.js";
+loadScript(scriptUrl);
 document.getElementById("submit").onclick = function() {
     var input = document.getElementById('urlInput');
     var submit = document.getElementById('submit');
-    var particl = document.getElementById('tsparticles')
+    var particl = document.getElementById('tsparticles');
     var ifr = document.getElementById('ifr');
     var cmp = document.getElementById('compatibilityBox');
     input.visibility = 'hidden';
@@ -19,24 +19,4 @@ document.getElementById("submit").onclick = function() {
     ifr.visibility = 'visible';
     ifr.style = 'position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;';
    ifr.src = inputVal;
-
- /*   var xhr = new XMLHttpRequest();
-
-xhr.open('GET', inputVal);
-xhr.onreadystatechange = handler;
-xhr.responseType = 'blob';
-xhr.setRequestHeader('origin', 'foxxy.lol');
-xhr.send();
-
-function handler() {
-  if (this.readyState === this.DONE) {
-    if (this.status === 200) {
-      // this.response is a Blob, because we set responseType above
-      var data_url = URL.createObjectURL(this.response);
-      document.querySelector('#ifr').src = data_url;
-    } else {
-      console.error('woops');
-    }
-  }
-}*/
-}
+};
