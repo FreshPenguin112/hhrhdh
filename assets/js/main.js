efd223e4066a35c5e55deec88d93b4ce63d7cde4
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-02-13 08:31:32 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 window.onbeforeunload=function(){return '';};
-if(frames.length){alert('Sorry, doesn\'t work in frames.');}else{while((es=document.getElementsByTagName('script')).length){es[0].parentNode.removeChild(es[0]);}es=document.all;for(i=0;i<es.length;++i){e=es[i];for(p in e){if(!p.indexOf('on')&&e[p]){e[p]=null;}}}document.write(document.documentElement.outerHTML);document.close();onerror=function(){return true}}//4umi.com
 document.getElementById("submit").onclick = function() {
     var input = document.getElementById('urlInput');
     var submit = document.getElementById('submit');
@@ -13,5 +12,5 @@ document.getElementById("submit").onclick = function() {
                 inputVal = input.value;
     ifr.visibility = 'visible';
     ifr.style = 'position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;';
-   ifr.src = "https://corsproxy.io/?" + inputVal;
+   ifr.src = "https://proxier-freshpenguin112.vercel.app/" + inputVal;
 };
