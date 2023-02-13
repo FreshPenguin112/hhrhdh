89582bafccd7b4988a306cd62b191b8f02b7f48e
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-02-13 09:03:17 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,5 +12,5 @@ document.getElementById("submit").onclick = function() {
                 inputVal = input.value;
     ifr.visibility = 'visible';
     ifr.style = 'position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;';
-   ifr.src = "https://proxier-freshpenguin112.vercel.app/" + inputVal;
+   ifr.src = "https://api.allorigins.win/raw?url=" + inputVal;
 };
